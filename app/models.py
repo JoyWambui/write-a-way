5175b84dfc6c5669cd71aa7f4b778641a445ee8d
JoyWambui <joywambuindegwa@gmail.com>
2021-11-13 17:22:54 +0300

    Create user model

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    """Class that defines the User Model and its methods."""
+    __tablename__='users'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(25), unique=True,nullable=False)
+    user_email = db.Column(db.String(120), unique=True,nullable=False)
+    #user_bio = db.Column(db.String(25), unique=True,nullable=False)
+    #image= db.Column(db.String(25), unique=True,nullable=False)
+    #user_posts= db.relationship('Post',backref='author',lazy=True)
+    def __repr__(self):
+        return f"User('{self.username}','{self.user_email}')"
+    
+# class Post(db.Model):
+#     """Class that defines the Blog Post Model and its methods."""
+#     __tablename__='posts'
+    
+#     id = db.Column(db.Integer,primary_key=True)
+#     post_title= db.Column(db.String(60),nullable=False)
+#     post_content= db.Column(db.Text,nullable=False)
+#     post_creation = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+#     def __repr__(self):
+#         return f"Post('{self.post_title}','{self.post_creation}')"
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+
+
 class Config:
     """Parent class configurations"""
-    SQLALCHEMY_DATABASE_URI='postgres+psycopg2://bobo:Riptide@localhost/write_a_way'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://bobo:Riptide@localhost/write_a_way'
 
 class ProdConfig(Config):
     """Child class production configurations."""
