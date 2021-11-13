3f71a74c3eb05b06469a01e8adf52b76b2dd84b3
JoyWambui <joywambuindegwa@gmail.com>
2021-11-13 23:28:07 +0300

    Create logout feature

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User
 from .. import db
@@ -27,3 +27,9 @@ def login():
         flash('Invalid email or password!')
     title= 'Writer Login'
     return render_template('auth/login.html',login_form=login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    login_user()
+    return redirect(url_for('.index'))
